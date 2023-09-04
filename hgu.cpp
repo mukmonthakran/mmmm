390d318997441391670ec971a5ee38d8df5d0bda
mukmonthakran <139094738+mukmonthakran@users.noreply.github.com>
2023-09-04 10:48:31 +0700

    Add files via upload

---
+++ hgu.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+void CalCircle();
+void Rectangle();
+
+int main()
+{
+		char Ch;
+	do{
+		
+		cout << "Program Calculate of Circle." <<endl;
+		cout << "***************************" <<endl;
+		cout << "1. Circle"<<endl;
+		cout << "2. Rectangle"<<endl;
+		cout << "3. Exti"<<endl;
+		cout << "Choose Menu :";
+		cin >> Ch;
+		if(Ch == '1') CalCircle();
+			else if (Ch == '2')Rectangle();
+				else if (Ch == '3')cout << "Exit"<<endl;
+				else cout << "Wrong Menu"<<endl;
+
+	}while(Ch != '3');
+	system("pause");
+	return(0);
+}
+void CalCircle()
+{
+	float Radius, Area, Circumference;
+	cout << "input radius : ";
+	cin >> Radius;
+	Area = 3.14f*Radius*Radius;
+	Circumference = 2*3.14*Radius;;
+	cout << endl;
+	cout << "Area of circle : " << Area << endl;
+	cout << "Circumference of circle : " << Circumference << endl;
+	cout << "Diameter of circle : " << (2*Radius) << endl;
+}
+void Rectangle()
+{
+	float w,l,Area;
+	cout <<"Input width :";
+	cin >> w;
+	cout <<"Input long :";
+	cin >> l;
+	Area = w*l;
+	cout <<endl;
+	cout <<"Area of Rectangle : "<< Area <<endl;
+}
